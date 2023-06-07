d11193abfc302f6ce5a385a69eafed43d691dcaa
Anisa Braun <125165703+anisabmarie@users.noreply.github.com>
2023-06-06 20:32:53 -0600

    Add files via upload

---
+++ Project 3 - SQL Database.sql
@@ -0,0 +1,21 @@
+create table consumer_pricing(
+series_id varchar(50),
+year varchar(50),
+period varchar(50),
+value varchar(50),
+footnotes varchar(50)
+)
+
+select * from consumer_pricing
+
+
+create table producer_pricing(
+series_id varchar(50),
+year varchar(50),
+period varchar(50),
+value varchar(50),
+footnotes varchar(50)
+)
+select * from producer_pricing
+
+
