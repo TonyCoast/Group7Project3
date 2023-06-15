9c4642f1bb3d2a7ec8cbb6f9dac4348e5cebaa84
Nels <nels.jacobson2@gmail.com>
2023-06-14 19:30:13 -0600

    fixed app2.py

---
+++ app2.py
@@ -0,0 +1,103 @@
+import os
+import json
+import pandas as pd
+from flask import Flask, jsonify
+
+#################################################
+# Data Setup
+#################################################
+
+# Get the base directory of the project
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Define the file paths for the CSV files
+file_paths = {
+    'Alcohol_Atlanta': os.path.join(base_dir, 'CPI_Data', 'Alcohol_Atlanta.csv'),
+    'Alcohol_Chicago': os.path.join(base_dir, 'CPI_Data', 'Alcohol_Chicago.csv'),
+    'Alcohol_Denver': os.path.join(base_dir, 'CPI_Data', 'Alcohol_Denver.csv'),
+    'Dairy_Atlanta': os.path.join(base_dir, 'CPI_Data', 'Dairy_Atlanta.csv'),
+    'Dairy_Chicago': os.path.join(base_dir, 'CPI_Data', 'Dairy_Chicago.csv'),
+    'Dairy_Denver': os.path.join(base_dir, 'CPI_Data', 'Dairy_Denver.csv'),
+    'Meats_Atlanta': os.path.join(base_dir, 'CPI_Data', 'Meats_Atlanta.csv'),
+    'Meats_Chicago': os.path.join(base_dir, 'CPI_Data', 'Meats_Chicago.csv'),
+    'Meats_Denver': os.path.join(base_dir, 'CPI_Data', 'Meats_Denver.csv'),
+    'National_CPI': os.path.join(base_dir, 'CPI_PPI_Nationally', 'Merged_CPI_Data.csv'),
+    'National_PPI': os.path.join(base_dir, 'CPI_PPI_Nationally', 'Merged_PPI_Data.csv')
+}
+
+# Load the CSV files and convert to JSON
+json_data = {}
+for data_name, file_path in file_paths.items():
+    df = pd.read_csv(file_path)
+    json_data[data_name] = df.to_dict(orient='records')
+
+#################################################
+# Flask Setup
+#################################################
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return (
+        f"Welcome to the CPI Data API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/Alcohol_Atlanta - CPI for alcohol in Atlanta<br/>"
+        f"/api/v1.0/Alcohol_Chicago - CPI for alcohol in Chicago<br/>"
+        f"/api/v1.0/Alcohol_Denver - CPI for alcohol in Denver<br/>"
+        f"/api/v1.0/Dairy_Atlanta - CPI for dairy in Atlanta<br/>"
+        f"/api/v1.0/Dairy_Chicago - CPI for dairy in Chicago<br/>"
+        f"/api/v1.0/Dairy_Denver - CPI for dairy in Denver<br/>"
+        f"/api/v1.0/Meats_Atlanta - CPI for meats in Atlanta<br/>"
+        f"/api/v1.0/Meats_Chicago - CPI for meats in Chicago<br/>"
+        f"/api/v1.0/Meats_Denver - CPI for meats in Denver<br/>"
+        f"/api/v1.0/National_CPI - National CPI data<br/>"
+        f"/api/v1.0/National_PPI - National PPI data<br/>"
+    )
+
+@app.route("/api/v1.0/Alcohol_Atlanta")
+def alcohol_atlanta():
+    return jsonify(json_data['Alcohol_Atlanta'])
+
+@app.route("/api/v1.0/Alcohol_Chicago")
+def alcohol_chicago():
+    return jsonify(json_data['Alcohol_Chicago'])
+
+@app.route("/api/v1.0/Alcohol_Denver")
+def alcohol_denver():
+    return jsonify(json_data['Alcohol_Denver'])
+
+@app.route("/api/v1.0/Dairy_Atlanta")
+def dairy_atlanta():
+    return jsonify(json_data['Dairy_Atlanta'])
+
+@app.route("/api/v1.0/Dairy_Chicago")
+def dairy_chicago():
+    return jsonify(json_data['Dairy_Chicago'])
+
+@app.route("/api/v1.0/Dairy_Denver")
+def dairy_denver():
+    return jsonify(json_data['Dairy_Denver'])
+
+@app.route("/api/v1.0/Meats_Atlanta")
+def meats_atlanta():
+    return jsonify(json_data['Meats_Atlanta'])
+
+@app.route("/api/v1.0/Meats_Chicago")
+def meats_chicago():
+    return jsonify(json_data['Meats_Chicago'])
+
+@app.route("/api/v1.0/Meats_Denver")
+def meats_denver():
+    return jsonify(json_data['Meats_Denver'])
+
+@app.route("/api/v1.0/National_CPI")
+def national_cpi():
+    return jsonify(json_data['National_CPI'])
+
+@app.route("/api/v1.0/National_PPI")
+def national_ppi():
+    return jsonify(json_data['National_PPI'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
