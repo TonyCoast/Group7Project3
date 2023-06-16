f6f8091d88d67484fb1c430a6fb7d167b3e0ef2a
Anisa Braun <125165703+anisabmarie@users.noreply.github.com>
2023-06-15 20:29:23 -0600

    Add files via upload

---
+++ Cities DB.sql
@@ -0,0 +1,30 @@
+create table cpi_atl(
+series_id varchar(50),
+value varchar(50),
+period varchar(50),
+label varchar(50),
+dairy varchar (50),
+alcohol varchar(50),
+meats varchar(50)
+)
+
+
+create table cpi_chicago(
+series_id varchar(50),
+value varchar(50),
+period varchar(50),
+label varchar(50),
+dairy varchar (50),
+alcohol varchar(50),
+meats varchar(50)
+)
+
+create table cpi_denver(
+series_id varchar(50),
+value varchar(50),
+period varchar(50),
+label varchar(50),
+dairy varchar (50),
+alcohol varchar(50),
+meats varchar(50)
+)
